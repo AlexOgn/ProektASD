1ef6d1b14965dc0963fe3646324b0f8ab9a82f45
Alex Ognyanov <61375132+AlexOgn@users.noreply.github.com>
2022-06-08 23:23:18 +0300

    pathing.c -> terrain.c
    vsichko stana v edin fail boli me glavata ama tva bachka

--- terrain.c
+++ terrain.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define sizeX 8
-#define sizeY 8
+#define sizeX 1024
+#define sizeY 1024
+#define diff 5
 
 //grid i buffergrid, koeto shte polzvame za smooth()
 //steps - kolko puti da se smooth-ne terena
 //E - range na random
 float grid[sizeX][sizeY];
 float bgrid[sizeX][sizeY];
-int steps = 6;
+int steps = 8;
 int E = 60;
 
 //nqkva random funkciq deto vzeh ot neta shtoto bqh mnogo
@@ -111,6 +112,175 @@ void ds(int sx, int sy, int size){
     E+=3;
 }
 
+
+//////////////////////////TUMOR/////////////////////////////
+//////////////////////////TUMOR/////////////////////////////
+//////////////////////////TUMOR/////////////////////////////
+//////////////////////////TUMOR/////////////////////////////
+//////////////////////////TUMOR/////////////////////////////
+//////////////////////////TUMOR/////////////////////////////
+//////////////////////////TUMOR/////////////////////////////
+//////////////////////////TUMOR/////////////////////////////
+//////////////////////////TUMOR/////////////////////////////
+typedef struct Cell{
+    int x;
+    int y;
+} Cell;
+
+typedef struct QNode{
+    Cell cell;
+    struct QNode* next;
+} QNode;
+
+typedef struct Queue{
+    struct QNode *front, *rear;
+} Queue;
+
+QNode* node_init(int x, int y){
+    struct QNode* temp = (struct QNode*)malloc(sizeof(struct QNode));
+    struct Cell cell = {x, y};
+    temp->cell = cell;
+    temp->next = NULL;
+    return temp;
+}
+
+struct Queue* queue_init(){
+    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+void enQueue(struct Queue* q, int x, int y){
+    struct QNode* temp = node_init(x, y);
+
+    if (q->rear == NULL) {
+        q->front = q->rear = temp;
+        return;
+    }
+
+    q->rear->next = temp;
+    q->rear = temp;
+}
+
+Cell deQueue(struct Queue* q){
+    if (q->front == NULL)
+        return;
+
+    struct QNode* temp = q->front;
+    Cell cell = temp->cell;
+
+    q->front = q->front->next;
+
+    if (q->front == NULL){
+        q->rear = NULL;
+    }
+
+    free(temp);
+
+    return cell;
+}
+
+int queue_is_empty(struct Queue* q){
+    return (q->front == NULL && q->rear == NULL);
+}
+
+double distance(int x, int y, int x1, int y1){
+    return sqrt((x-x1)*(x-x1) + (y-y1) * (y-y1));
+}
+
+double get_diff(int x1, int y1, int x2, int y2){
+    return abs(grid[x1][y1] - grid[x2][y2]);
+}
+
+int is_water(int x, int y){
+    return (grid[x][y] < 75 && grid[x][y] > 0);
+}
+
+void find_path(int startX, int startY, int endX, int endY){
+    int** visited = calloc(sizeof(int*), sizeX);
+    Cell** prev = calloc(sizeof(Cell*), sizeX);
+    for(int i=0;i<sizeX;i++){
+        visited[i] = calloc(sizeY, sizeof(int));
+        prev[i] = calloc(sizeY, sizeof(Cell));
+    }
+
+    for(int i=0;i<sizeX;i++){
+        for(int j=0;j<sizeY;j++){
+            prev[i][j].x = -1;
+            prev[i][j].y = -1;
+        }
+    }
+
+    Queue *q = queue_init();
+    enQueue(q, startX, startY);
+    visited[startX][startY] = 1;
+
+    while(!queue_is_empty(q)){
+        Cell node = deQueue(q);
+        int x = node.x;
+        int y = node.y;
+
+        if(x-1 >= 0 && visited[x-1][y] == 0 && get_diff(x, y, x-1, y) <= diff && is_water(x-1, y) == 0){
+            visited[x-1][y] = 1;
+            enQueue(q, x-1, y);
+
+            Cell cell = {x, y};
+            prev[x-1][y] = cell;
+        }
+
+        if(x+1 < sizeX && visited[x+1][y] == 0 && get_diff(x, y, x+1, y) <= diff && is_water(x+1, y) == 0){
+            visited[x+1][y] = 1;
+            enQueue(q, x+1, y);
+
+            Cell cell = {x, y};
+            prev[x+1][y] = cell;
+        }
+
+        if(y-1 >= 0 && visited[x][y-1] == 0 && get_diff(x, y, x, y-1) <= diff && is_water(x, y-1) == 0){
+            visited[x][y-1] = 1;
+            enQueue(q, x, y-1);
+
+            Cell cell = {x, y};
+            prev[x][y-1] = cell;
+        }
+
+        if(y+1 < sizeY && visited[x][y+1] == 0 && get_diff(x, y, x, y+1) <= diff && is_water(x, y+1) == 0){
+            visited[x][y+1] = 1;
+            enQueue(q, x, y+1);
+
+            Cell cell = {x, y};
+            prev[x][y+1] = cell;
+        }
+    }
+
+    free(q);
+
+    Cell* path = calloc(sizeX, sizeof(Cell));
+    Cell at = {endX, endY};
+    int index = 0;
+    int capacity = sizeX;
+
+    for(at; at.x != -1; at = prev[at.x][at.y]){
+        if(index == capacity-1){
+            capacity *= 2;
+            path = realloc(path, capacity * sizeof(Cell));
+        }
+
+        path[index] = at;
+        index++;
+    }
+
+    for(int i=0;i<index/2;i++){
+        Cell t = path[i];
+        path[i] = path[index-i-1];
+        path[index-i-1] = t;
+    }
+
+    for(int i=0;i<index;i++){
+        grid[path[i].x][path[i].y] = -1;
+    }
+}
+
 int main(){
     //seed na rand(hint: kato e v ds ne bachka ama nikak)
     srand((unsigned) time(0));
@@ -137,6 +307,8 @@ int main(){
         smooth();
     }
 
+    find_path(0, 0, 0, 1000);
+
     //otvarq se faila
     FILE * terrain = fopen("Terrain.ppm","wb");
 
@@ -149,17 +321,19 @@ int main(){
     for(int i = 0; i < sizeX; i++){
         for(int j = 0; j < sizeY; j++){
                 int k = grid[j][i];
-                printf("%d ",k);
                 if(k>305){
                     k=305;
                 }
-                if(k < 75)
+                if(k == -1){
+                    ///puteka
+                    fprintf(terrain,"%d %d %d ", 0,0,0);
+                }else if(k < 75)
                     ///0 - 75 sinio
                     fprintf(terrain,"%d %d %d ", 0,0,abs(255-abs(k)));
                 else if(k < 160)
                     ///75 - 160 zeleno
                     fprintf(terrain,"%d %d %d ", 0,k,0);
-                else if(k < 235)
+                else if(k < 233)
                     ///160 - 230 kafqvo
                     fprintf(terrain,"%d %d %d ", k-30,k/2-30,0);
                 else {
